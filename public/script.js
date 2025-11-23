e96d0f772356400f2869143ed24ab75e5d61e3c1
Yuva Dhanvanth <yuvadhanvanth842007@gmail.com>
2025-11-23 22:22:00 +0530

    again

--- public/script.js
+++ public/script.js
@@ -456,7 +456,7 @@ function setupControls() {
     aiImagePreview.src = "";
 
     try {
-      const res = await fetch('https://02efcd695e7c3d.lhr.life/predict', {
+      const res = await fetch(' https://2104e240758403.lhr.life/predict', {
         method: "POST",
         body: formData
       });
@@ -480,7 +480,7 @@ function setupControls() {
 
       // FIXED: Show uploaded image using the correct endpoint
       // ✅ CORRECT - use your public AI URL
-      aiImagePreview.src = "https://02efcd695e7c3d.lhr.life/uploaded_image?" + new Date().getTime();
+      aiImagePreview.src = " https://2104e240758403.lhr.lifeuploaded_image?" + new Date().getTime();
       aiImagePreview.style.display = "block";
 
     } catch (err) {
